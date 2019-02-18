e2e4d6952b91b0f9b948529badea8c7441001796
angelq87 <angelquintero87@gmail.com>
2019-02-18 13:47:33 -0500

    Solo probe si bootstrap funciona

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Button } from 'reactstrap';
+
 
 class App extends Component {
   render() {
+    
+    
+
     return (
       <div className="App">
         <header className="App-header">          
           <p>
             AQuinsoft Solutions
           </p>         
-        </header>
+        </header>       
       </div>
     );
   }
