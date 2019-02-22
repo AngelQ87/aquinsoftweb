59b0a6a907d553b1c8d742200a8b44a8065233b0
AngelQ87 <angelquintero87@gmail.com>
2019-02-21 23:09:47 -0500

    puesto fondo de imagen en el body

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navigationbar from './components/Navigationbar';
 import Footer from './components/Footer';
 import Inicio from './components/Inicio';
 import Startup from './components/Startup';
 import Contacto from './components/Startup';
+import img from './images/indice.jpg';
 
 
 class App extends Component {
   render() {
-    
-    
+
+
 
     return (
-      <div className="App"> 
-        <Navigationbar/>           
-        <header className="App-header">                          
-          <p>
-            AQuinsoft Solutions
+      <div className="App">
+        <Navigationbar />
+        <div className="container-fluid" id="body">
+          <div className="row">
+          <div className="col-md-6">
+            <p>
+              AQuinsoft Solutions
           </p>
-          <img src="./images/DSC_0428.JPG"/>                   
-        </header>
-        <Footer/>       
+          </div>
+          <div className="col-md-6">
+            <p>lorem isu impom</p>
+          </div>
+          </div>
+        </div>
+        <Footer/>
       </div>
     );
   }
