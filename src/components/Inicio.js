a5c9eaa161e957ad7a27fc0bb60aab26227089e8
AngelQ87 <angelquintero87@gmail.com>
2019-02-27 23:02:40 -0500

    ya esta el diseno de la pagina, falta animaciones y texto real

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -3,19 +3,20 @@ import './inicio.css';
 import Navigationbar from './Navigationbar';
 import Footer from './Footer';
 import imagen from '../images/44.jpg';
+import imagenidea from '../images/83.jpg';
 
 
 class Inicio extends Component {
   render() {
 
-
+    
 
     return (
       <div className="container-fluid">
-        <div className="row">
+        <div className="row" id="home">
           <Navigationbar />
         </div>
-        <div className="row inicio">
+        <div className="row inicio mt-5">
           <div className="col-md-6 polaroid rotate_left rounded">
             <img src={imagen} className="img-fluid" />
             <p>
@@ -52,7 +53,7 @@ class Inicio extends Component {
                   del cliente como suyas donde creamos productos
                   profesionales de alta calidad para un mercado
                   excepcionalmente competitivo.
-        </p>
+                </p>
               </div>
             </div>
             <div className="col-md-3"></div>
@@ -60,15 +61,63 @@ class Inicio extends Component {
 
         </div>
         <div className="row">
+          <section id="section1">
+            <div className="container">
+              <p className="h3">
+                Somos una startup joven que vive las necesidades
+                del cliente como suyas donde creamos productos
+                profesionales de alta calidad para un mercado
+                excepcionalmente competitivo. Somos una startup joven que vive las necesidades
+                del cliente como suyas donde creamos productos
+                profesionales de alta calidad para un mercado
+                excepcionalmente competitivo. Somos una startup joven que vive las necesidades
+                del cliente como suyas donde creamos productos
+                profesionales de alta calidad para un mercado
+                excepcionalmente competitivo.
+          </p>
+            </div>
+          </section>
+        </div>
+        <div className="row">
+          <section id="contacto">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-6">
+                  <form className="card">
+                    <div className="card-body">
+                      <div className="form-group">
+                        <input className="form-control" type="text" placeholder="Nombre" />                        
+                      </div>
+                      <div className="form-group">
+                        <input className="form-control" type="email" placeholder="Email" />                        
+                      </div>
+                      <div className="form-group">
+                        <textarea className="form-control" type="text" rows="10" cols="50" placeholder="Mensaje"/>                        
+                      </div>
+                      <button onSubmit="" className="btn btn-outline-secondary">Enviar</button>
+                    </div>
+                  </form>
+                </div>
+                <div className="col-md-6 my-auto">
+                 <img src={imagenidea} className="img-fluid rounded-circle"/>
+                </div>
+
+              </div>
+            </div>
+          </section>
+        </div>
+        <div className="row">
           <Footer />
         </div>
 
-
+        
       </div>
-
-
+      
+     
+      
     );
   }
 }
 
 export default Inicio;
+  
--- src/components/Navigationbar.js
+++ src/components/Navigationbar.js
@@ -9,7 +9,7 @@ class Navigationbar extends Component {
     render() {
         return (
             <div className="navigation">
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
                 <div className="container">
                     <a className="navbar-brand" href="#">AQuinsoft Solutions</a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -18,10 +18,10 @@ class Navigationbar extends Component {
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav ml-auto">                        
                             <li className="nav-item active">                                
-                                <a className="nav-link" href="/">Inicio</a>
+                                <a className="nav-link" href="#home">Inicio</a>
                             </li>                            
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Contacto</a>
+                                <a className="nav-link" href="#contacto">Contacto</a>
                             </li>
                         </ul>
                     </div>
